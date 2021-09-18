8d96010fa2814a9fb3bc334501f12272bdd87c5b
Jacob A <jacobraldridge@gmail.com>
2021-09-17 22:51:20 -0400

    Added scrolling

--- ProceduralGen.java
+++ ProceduralGen.java
@@ -3,9 +3,9 @@ import java.awt.event.*;
 
 public class ProceduralGen extends Canvas implements KeyListener
 {        
-    private final int HEIGHT = 800;
-    private final int WIDTH = 1300;
-    private final int PIXEL_SIZE = 5;
+    private final int HEIGHT = 700;
+    private final int WIDTH = 700;
+    private final int PIXEL_SIZE = 10;
 
     private final double NOISE_ZOOM = 1.0;
 
@@ -21,13 +21,15 @@ public class ProceduralGen extends Canvas implements KeyListener
         frame.setSize(WIDTH, HEIGHT);    
         frame.setVisible(true); 
 
+        generateTerrainArray();
         repaint();
     }    
 
     public void paint(Graphics g) { }   
     
     public void update(Graphics g) {
-        generateTerrainArray();
+        //generateTerrainArray();
+        scroll();
         for (int i = 0; i < heights.length; i++) {
             for (int j = 0; j < heights[0].length; j++) {
                 int c = (int) (heights[i][j] * 255);
@@ -59,6 +61,33 @@ public class ProceduralGen extends Canvas implements KeyListener
             }
         }
     }
+
+    // Moves array up and to the left and generates new values in bottom and right edges
+    private void scroll() {
+        // Move array up and to the left
+        for (int i = 0; i < heights.length - 1; i++) {
+            for (int j = 0; j < heights[0].length - 1; j++) {
+                heights[i][j] = heights[i + 1][j + 1];
+            }
+        }
+        // Generate the bottom left and top right corners
+        heights[0][heights[0].length - 1] = generateNewHeight(heights[0][heights[0].length - 2]);
+        heights[heights.length - 1][0] = generateNewHeight(heights[heights.length - 2][0]);
+
+        // Generate the right edge and bottom right corner
+        for (int i = 1; i < heights.length; i++) {
+            double heightAbove = heights[i - 1][heights[0].length - 1];
+            double heightToLeft = heights[i][heights[0].length - 2];
+            heights[i][heights[0].length - 1] = generateNewHeight(heightAbove, heightToLeft);
+        }
+        // Generate the bottom edge
+        for (int i = 1; i < heights[0].length - 1; i++) {
+            double heightAbove = heights[heights.length - 2][i];
+            double heightToLeft = heights[heights.length - 1][i - 1];
+            heights[heights.length - 1][i] = generateNewHeight(heightAbove, heightToLeft);
+        }
+    }
+
     private double generateNewHeight(double referenceHeight) {
         double deltaHeight = Math.random() - 0.5;
         return referenceHeight + deltaHeight * ((referenceHeight - 1) * (referenceHeight));
