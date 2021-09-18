9cf911428d63b64007ca6a9671b1c0cedbb2f07b
Jacob A <jacobraldridge@gmail.com>
2021-09-17 22:05:45 -0400

    Colors are now always within 0 and 1
    There are still some huge patches of the same color but I might move one from that and work on scrolling

--- ProceduralGen.java
+++ ProceduralGen.java
@@ -1,23 +1,20 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Random;
 
 public class ProceduralGen extends Canvas implements KeyListener
 {        
     private final int HEIGHT = 800;
     private final int WIDTH = 1300;
-    private final int SQUARE_SIZE = 5;
+    private final int PIXEL_SIZE = 5;
 
-    private double[][] heights = new double[WIDTH / SQUARE_SIZE + 1][HEIGHT / SQUARE_SIZE + 1];
+    private final double NOISE_ZOOM = 1.0;
 
-    private int thing = 10;
+    private double[][] heights = new double[WIDTH / PIXEL_SIZE][HEIGHT / PIXEL_SIZE];
 
     public ProceduralGen()    
     {    
-        Frame frame = new Frame("Canvas Example");   
-        //TODO explore setSize() versus frame.setSize()
+        Frame frame = new Frame("Procedural Gen");   
         setBackground(Color.white);
-        setSize(WIDTH, HEIGHT);
         
         addKeyListener(this);
         frame.add(this);    
@@ -30,62 +27,47 @@ public class ProceduralGen extends Canvas implements KeyListener
     public void paint(Graphics g) { }   
     
     public void update(Graphics g) {
-        generateTerrainArray(3);
-        //normalizeHeightArray();
+        generateTerrainArray();
         for (int i = 0; i < heights.length; i++) {
             for (int j = 0; j < heights[0].length; j++) {
-                int c = (int) ((heights[i][j] + 5) * 30);
+                int c = (int) (heights[i][j] * 255);
                 g.setColor(new Color(c, c, c));
-                g.fillRect(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+                g.fillRect(i * PIXEL_SIZE, j * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
             }
         }
     }
 
-    private void generateTerrainArray(int octaves) {
-        Random r = new Random();
+    private void generateTerrainArray() {
         for (int i = 0; i < heights.length; i++) {
             for (int j = 0; j < heights[0].length; j++) {
                 // If not on the top and left borders
                 if (i > 0 && j > 0) {
-                    double avgHeight = (heights[i][j-1] + heights[i-1][j]) / 2;
-                    heights[i][j] = avgHeight + (r.nextGaussian() / 10);
+                    heights[i][j] = generateNewHeight(heights[i - 1][j], heights[i][j - 1]);
+                } 
                 // If on the top border
-                } else if (i > 0) {
-                    heights[i][j] = heights[i-1][j] + (r.nextGaussian() / 10);
-                // If on the right border
-                } else if (j > 0) {
-                    heights[i][j] = heights[i][j-1] + (r.nextGaussian() / 10);
+                else if (i > 0) {
+                    heights[i][j] = generateNewHeight(heights[i - 1][j]);
+                } 
+                // If on the right border 
+                else if (j > 0) {
+                    heights[i][j] = generateNewHeight(heights[i][j - 1]);
                 }
                 // If in the corner start with a random value
                 if (i == 0 && j == 0) {
-                    heights[i][j] = r.nextDouble();
+                    heights[i][j] = Math.random();
                 }
             }
         }
     }
+    private double generateNewHeight(double referenceHeight) {
+        double deltaHeight = Math.random() - 0.5;
+        return referenceHeight + deltaHeight * ((referenceHeight - 1) * (referenceHeight));
+    }
 
-    // TODO fix this or make a better generation algorithm that doesn't need normalization
-    private void normalizeHeightArray() {
-        double high = 0;
-        double low = 0;
-        // Find highest and lowest value
-        for (int i = 0; i < heights.length; i++) {
-            for (int j = 0; j < heights[0].length; j++) {
-                double height = heights[i][j];
-                if(height > high) {
-                    high = height;
-                }
-                if(height < low) {
-                    low = height;
-                }
-            }
-        }
-        // Divide values by highest after adding the lowest value (because its negative)
-        for (int i = 0; i < heights.length; i++) {
-            for (int j = 0; j < heights[0].length; j++) {
-                heights[i][j] = (heights[i][j] - low) / high;
-            }
-        }
+    private double generateNewHeight(double heightOne, double heightTwo) {
+        double avgerageHeight = (heightOne + heightTwo) / 2;
+        double deltaHeight = Math.random() - 0.5;
+        return avgerageHeight + deltaHeight * ((avgerageHeight - 1) * (avgerageHeight));
     }
 
     public void keyTyped(KeyEvent e) { }
