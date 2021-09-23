4828ce331fbfc008fccbcc7bd8fd9b2f781217b4
Jacob A <jacobraldridge@gmail.com>
2021-09-23 01:00:56 -0400

    Getting a hold on the recursive regeneration algorithm
    I wrote the base program for making points but now I need to write the recursion

--- ProceduralGen.java
+++ ProceduralGen.java
@@ -2,19 +2,18 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.JFrame;
 
-// TODO Make a velocity tht changes which is for changing points rather than adding/subtracting random values
+// TODO Implement algorithm that takes midpoints of corners with added randomness until every pixel is colored
+// or make a velocity that changes which is for changing points rather than adding/subtracting random values
 
 public class ProceduralGen extends Canvas implements KeyListener
 {        
     // TODO these values are not very flexible so write a method that takes any
     // number and adjusts these values if they're not in proper form without messing
     // with the general idea
-    private final int PIXEL_SIZE = 50;
-    private final int NUM_PIXELS = 7; // Must be in the form 2^n - 1
+    // TODO Also NUM_PIXELS is confusing. Is it total or..?
+    private final int PIXEL_SIZE = 5;
+    private final int NUM_PIXELS = 65; // Must be in the form 2^n + 1
     private final int SIZE = PIXEL_SIZE * NUM_PIXELS;
-
-    // The higher the more zoomed out the noise is
-    private final double NOISE_ZOOM = 1.0;
 
     private double[][] heights = new double[SIZE / PIXEL_SIZE][SIZE / PIXEL_SIZE];
 
@@ -25,7 +24,7 @@ public class ProceduralGen extends Canvas implements KeyListener
         
         addKeyListener(this);
         frame.add(this);    
-        frame.setSize(SIZE, 800);    
+        frame.setSize(SIZE, SIZE);    
         frame.setVisible(true); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -52,12 +51,33 @@ public class ProceduralGen extends Canvas implements KeyListener
         heights[0][heights.length - 1] = Math.random();
         heights[heights.length - 1][heights.length - 1] = Math.random();
 
-        // Generate the midpoint
-        System.out.println("\n\n\nSTART OF BIG THING");
-        for(int i = 0; i < 2; i++) {
+        for(int i = 0; i < 0; i++) {
             int id = (int) (NUM_PIXELS / Math.pow(2, i));
-            generateRandomMidpoint(0, 0, id, id);
+            // Subtract one from id on first iteration to stay in bounds
+            id = i == 0 ? id - 1 : id;
+            generateRandomMidpoint(0, 0, 0, id);
         }
+        int id = NUM_PIXELS - 1;
+        splitChunk(0, 0, id, 0, 0, id, id, id);
+    }
+    
+    /**
+     * @param i1 col index of top left point
+     * @param j1 row index of top left point
+     * @param i2 col index of top right point
+     * @param j2 row index of top right point
+     * @param i3 col index of bottom left point
+     * @param j3 row index of bottom left point
+     * @param i4 col index of bottom right point
+     * @param j4 row index of bottom right point
+     */
+    private void splitChunk (int i1, int j1, int i2, int j2, int i3, int j3, int i4, int j4) {
+        // Make midpoints of the given points and a diagonal midpoint
+        generateRandomMidpoint(i1, j1, i2, j2);
+        generateRandomMidpoint(i1, j1, i3, j3);
+        generateRandomMidpoint(i4, j4, i2, j2);
+        generateRandomMidpoint(i4, j4, i3, j3);
+        generateRandomMidpoint(i1, j1, i4, j4);
     }
 
     private void generateRandomMidpoint(int i1, int j1, int i2, int j2) {
@@ -66,12 +86,10 @@ public class ProceduralGen extends Canvas implements KeyListener
         // Calculates the distance between the points and counts diagonal squares as one
         // If the points are diagonal, i or j is the diagonal squares
         // If they're not diagonal, use the sum of differences because one difference is 0
-        // int distBetweenPoints = (i1 - i2) == (j1 - j2) ? Math.abs(i1 - i2) : Math.abs((i1 - i2) + (j1 - j2));
-        heights[(i1 + i2) / 2][(j1 + j2) / 2] = avgHeight + (Math.random() * minDistToEdge);
-        System.out.println("\nSTART OF THING");
-        System.out.println("avgHeight " + avgHeight);
-        System.out.println("minDistToEdge " + minDistToEdge);
-        System.out.println("calculation " + (avgHeight + (Math.random() * minDistToEdge)));
+        int pixelDistanceToPoint = (i1 - i2) == (j1 - j2) ? Math.abs(i1 - i2) : Math.abs((i1 - i2) + (j1 - j2));
+        double scalingFactor = pixelDistanceToPoint / NUM_PIXELS;
+        double randomDelta = Math.random() * minDistToEdge;
+        heights[(i1 + i2) / 2][(j1 + j2) / 2] = avgHeight + (randomDelta * scalingFactor);
     }
 
     private Color generateColor(double height) {
