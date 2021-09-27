7c655432422105400b08079162b8a2972334cd16
Jacob A <jacobraldridge@gmail.com>
2021-09-26 21:10:17 -0400

    Realized the chunk generation method is flawed and threw it away
    Started implementing the velocity generation method instead which shouldn't have any issues with non uniform sections

--- ProceduralGen.java
+++ ProceduralGen.java
@@ -7,17 +7,15 @@ import javax.swing.JFrame;
 
 public class ProceduralGen extends Canvas implements KeyListener
 {        
-    // TODO these values are not very flexible so write a method that takes any
-    // number and adjusts these values if they're not in proper form without messing
-    // with the general idea
-    // TODO Also NUM_PIXELS is confusing. Is it total or..?
     private final int PIXEL_SIZE = 5;
-    private final int NUM_PIXELS = 129; // Must be in the form 2^n + 1
-    private final int SIZE = PIXEL_SIZE * NUM_PIXELS;
+    private final int WIDTH = 1000;
+    private final int HEIGHT = 600;
+
+    private final double MAX_NOISE_CHANGE = 0.2;
 
     //private final int NOISE_CONSTANT = 2;
 
-    private double[][] heights = new double[NUM_PIXELS][NUM_PIXELS];
+    private double[][] heights = new double[WIDTH / PIXEL_SIZE][HEIGHT / PIXEL_SIZE];
 
     public ProceduralGen()    
     {    
@@ -26,7 +24,7 @@ public class ProceduralGen extends Canvas implements KeyListener
         
         addKeyListener(this);
         frame.add(this);    
-        frame.setSize(SIZE, SIZE);    
+        frame.setSize(WIDTH, HEIGHT);    
         frame.setVisible(true); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -51,52 +49,20 @@ public class ProceduralGen extends Canvas implements KeyListener
     private void generateTerrainArray() {
         // Generate four corners
         heights[0][0] = Math.random();
-        heights[heights.length - 1][0] = Math.random();
-        heights[0][heights.length - 1] = Math.random();
-        heights[heights.length - 1][heights.length - 1] = Math.random();
-        int size = NUM_PIXELS - 1;
-        splitChunks(0, 0, size);
-    }
-    
-    /**
-     * Recursively generates the heights of a square of a given sideLength
-     * at a given rowId and colId by taking midpoints until completed
-     */
-    private void splitChunks (int colId, int rowId, int sideLength) {
-        // Stop at the pixel level
-        if(sideLength == 1) { return; }
-
-        // Points at the corners of the chunk
-        // Top right, top left, bottom left, and bottom right
-        int[] tLIds = {colId, rowId};
-        int[] tRIds = {colId + sideLength, rowId};
-        int[] bLIds = {colId, rowId + sideLength};
-        int[] bRIds = {colId + sideLength, rowId + sideLength};
-
-        // Make midpoints of the given points and a diagonal midpoint
-        generateRandomMidpoint(tRIds[0], tRIds[1], tLIds[0], tLIds[1]);
-        generateRandomMidpoint(bRIds[0], bRIds[1], bLIds[0], bLIds[1]);
-        generateRandomMidpoint(tRIds[0], tRIds[1], bRIds[0], bRIds[1]);
-        generateRandomMidpoint(tLIds[0], tLIds[1], bLIds[0], bLIds[1]);
-        generateRandomMidpoint(tRIds[0], tRIds[1], bLIds[0], bLIds[1]);
-
-        // Make chunks within this chunk
-        int halfLength = sideLength / 2;
-        splitChunks(colId, rowId, halfLength);
-        splitChunks(halfLength + colId, rowId, halfLength);
-        splitChunks(colId, halfLength + rowId, halfLength);
-        splitChunks(halfLength + colId, halfLength + rowId, halfLength);
+        double[] startVelocities = new double[heights.length];
+        startVelocities[0] = (2 * (Math.random() - 0.5)) * MAX_NOISE_CHANGE;
+        for(int i = 1; i < heights.length - 1; i++) {
+            startVelocities[i] = generateNextVelocity(heights[i - 1][0], startVelocities[i - 1]);
+            heights[i][0] += startVelocities[i - 1];
+        }
     }
 
-    private void generateRandomMidpoint(int i1, int j1, int i2, int j2) {
-        // Calculates the distance between the points and counts diagonal squares as one
-        // If the points are diagonal, i or j is the diagonal squares
-        // If they're not diagonal, use the sum of differences because one difference is 0
-        int pixelDistanceToPoint = (i1 - i2) == (j1 - j2) ? Math.abs(i1 - i2) : Math.abs((i1 - i2) + (j1 - j2));
-        double avgHeight = (heights[i1][j1] + heights[i2][j2]) / 2;
-        double minDistToEdge = Math.abs(avgHeight - 1) > avgHeight ? avgHeight : Math.abs(avgHeight - 1);
-        double randomDelta = 2 * (Math.random() - 0.5) * minDistToEdge; 
-        heights[(i1 + i2) / 2][(j1 + j2) / 2] = avgHeight + randomDelta;
+    private double generateNextVelocity(double currentHeight, double currentVel) {
+        // The smallest distance from being too big or too small
+        double minDistToMax = Math.abs(currentHeight - 1) > currentHeight ? currentHeight  : Math.abs(currentHeight - 1);
+        double randomDelta = (2 * (Math.random() - 0.5)) * minDistToMax * MAX_NOISE_CHANGE;
+        return currentHeight + randomDelta;
+
     }
 
     private Color generateColor(double height) {
