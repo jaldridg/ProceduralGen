38b5b79f45147e34c5d2b15f12d1caf3e6250148
Jacob A <jacobraldridge@gmail.com>
2021-12-29 17:48:40 -0500

    Stripe terrain!

--- CanvasMap.java
+++ CanvasMap.java
@@ -15,6 +15,9 @@ public class CanvasMap extends Canvas {
     private double[][] heights = new double[WIDTH / PIXEL_SIZE][HEIGHT / PIXEL_SIZE];
     private Color[][] colors = new Color[WIDTH / PIXEL_SIZE][HEIGHT / PIXEL_SIZE];
 
+    private Image mapImage = null;
+    private Graphics2D g2d;
+
     public CanvasMap() {
         generate();        
 
@@ -32,9 +35,43 @@ public class CanvasMap extends Canvas {
     } 
 
     private void generateHeightArray() {
+        // Generate first pixel
+        heights[0][0] = 0.5;
+        // Generate top
+        for (int i = 1; i < heights.length; i++) {
+            heights[i][0] = heights[i - 1][0] + rng.nextGaussian();
+        }
+        // Generate left
+        for (int i = 1; i < heights[0].length; i++) {
+            heights[0][i] = heights[0][i - 1] + rng.nextGaussian();
+        }
+        // Generate middle
+        for (int i = 1; i < heights.length; i++) {
+            for (int j = 1; j < heights[i].length; j++) {
+                double average = (heights[i - 1][j] + heights[i][j - 1]) / 2;
+                heights[i][j] = average + rng.nextGaussian();
+            }
+        }
+        normalizeHeightArray();
+    }
+
+    private void normalizeHeightArray() {
+        // Find max and min
+        double max = heights[0][0];
+        double min = heights[0][0];
         for (int i = 0; i < heights.length; i++) {
             for (int j = 0; j < heights[i].length; j++) {
-                heights[i][j] = rng.nextDouble();
+                if (heights[i][j] > max) {
+                    max = heights[i][j];
+                } else if (heights[i][j] < min) {
+                    min = heights[i][j];
+                }
+            }
+        }
+        //Normalize
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = 0; j < heights[i].length; j++) {
+                heights[i][j] = (heights[i][j] - min) / (Math.abs(min) + Math.abs(max));
             }
         }
     }
@@ -54,12 +91,16 @@ public class CanvasMap extends Canvas {
         if (colors == null) {
             generateColorArray();
         }
+        mapImage = createImage(WIDTH, HEIGHT);
+        g2d = (Graphics2D) mapImage.getGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         for (int i = 0; i < colors.length; i++) {
             for (int j = 0; j < colors[i].length; j++) {
-                g.setColor(colors[i][j]);
-                g.fillRect(i * PIXEL_SIZE, j * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+                g2d.setColor(colors[i][j]);
+                g2d.fillRect(i * PIXEL_SIZE, j * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
             }
         }
+        g.drawImage(mapImage, 0, 0, null);        
     }
 
     public int getSeed() {
